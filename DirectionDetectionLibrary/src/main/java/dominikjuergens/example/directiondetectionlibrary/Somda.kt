357ddff7a2d5bf5c984b33b603e14444762bc76e
Dominik <dominik.juergens85@gmail.com>
2022-12-04 20:02:40 +0100

    direction detection library added

---
+++ DirectionDetectionLibrary/src/main/java/dominikjuergens/example/directiondetectionlibrary/Somda.kt
@@ -0,0 +1,4 @@
+package dominikjuergens.example.directiondetectionlibrary
+
+class Somda {
+}
---
+++ app/src/main/java/dominikjuergens/example/directiondetectionexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package dominikjuergens.example.directiondetectionexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
