51e7195fcf4460af669fce4fb0a6c0d916276bc7
Dominik <dominik.juergens85@gmail.com>
2022-12-08 19:40:46 +0100

    -changed to an Interface solution

--- DirectionDetectionLibrary/src/main/java/dominikjuergens/example/directiondetectionlibrary/Somda.kt
+++ DirectionDetectionLibrary/src/main/java/dominikjuergens/example/directiondetectionlibrary/Somda.kt
@@ -1,64 +1,79 @@
 package dominikjuergens.example.directiondetectionlibrary
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 
-class Somda {
+class Somda(context: Context) {
 
-    companion object {
+    private var callback: SomdaListener? = null
 
-        private var azimuth: Float = 0F
-        private var pitch: Float = 0F
-        private var roll: Float = 0F
-        private var zAcc: Float = 0F
+    fun start(onInteractionListener: SomdaListener) {
+        callback = onInteractionListener
+        // TODO do your lib stuff
+        callback?.onSomdaChanged(42.0F)
+    }
 
-        /**
-         * takes the fragments sensorManager, the fragment as the sensorEventListener and the
-         * sensorEvent from the onSensorChanged function
-         */
-        fun doSomda(mSensorManager: SensorManager, sensorEventListener: SensorEventListener,
-                    event: SensorEvent): Float {
-            //get sensor values
-            getSensorValues(event)
-            //roll correction
-            val correctedAzimuth = rollCorrection()
-            //TODO windowed peak algorithm for further correction
-            return 42F
+    fun stop() {
+        callback = null
+        // TODO stop your lib stuff
+    }
+
+    interface SomdaListener {
+        fun onSomdaChanged(degree: Float)
+    }
+
+    private var azimuth: Float = 0F
+    private var pitch: Float = 0F
+    private var roll: Float = 0F
+    private var zAcc: Float = 0F
+
+    /**
+     * takes the fragments sensorManager, the fragment as the sensorEventListener and the
+     * sensorEvent from the onSensorChanged function
+     */
+    fun doSomdaAlgorithm(mSensorManager: SensorManager, sensorEventListener: SensorEventListener,
+                event: SensorEvent): Float {
+        //get sensor values
+        getSensorValues(event)
+        //roll correction
+        val correctedAzimuth = rollCorrection()
+        //TODO windowed peak algorithm for further correction
+        return 42F
+    }
+
+    private fun getSensorValues(event: SensorEvent){
+        if(event.sensor.type == Sensor.TYPE_ROTATION_VECTOR) { //get euler angles
+            val orientations = DirectionSensors.getEulerAngles(event)
+            azimuth = orientations.first
+            pitch = orientations.second
+            roll = orientations.third
+        } else if(event.sensor.type == Sensor.TYPE_LINEAR_ACCELERATION) { //get linear z-Axis Acceleration
+            zAcc = DirectionSensors.getZAcceleration(event)
         }
+    }
 
-        private fun getSensorValues(event: SensorEvent){
-            if(event.sensor.type == Sensor.TYPE_ROTATION_VECTOR) { //get euler angles
-                val orientations = DirectionSensors.getEulerAngles(event)
-                azimuth = orientations.first
-                pitch = orientations.second
-                roll = orientations.third
-            } else if(event.sensor.type == Sensor.TYPE_LINEAR_ACCELERATION) { //get linear z-Axis Acceleration
-                zAcc = DirectionSensors.getZAcceleration(event)
-            }
+    private fun rollCorrection(): Float {
+        if(pitch < 0) {
+            return (calculateAngle(azimuth + roll))
+        } else { // (pitch >= 0)
+            return (calculateAngle(azimuth - roll))
         }
+    }
 
-        private fun rollCorrection(): Float {
-            if(pitch < 0) {
-                return (calculateAngle(azimuth + roll))
-            } else { // (pitch >= 0)
-                return (calculateAngle(azimuth - roll))
-            }
-        }
-
-        /**
-         * because angles can only be between 0 and 359, it has to be checked for undefined angles
-         * after the addition of two angles
-         */
-        private fun calculateAngle(angle: Float): Float {
-            if(angle >= 360) {
-                return (angle - 360)
-            } else if (angle < 0) {
-                return (angle + 360)
-            } else {
-                return angle
-            }
+    /**
+     * because angles can only be between 0 and 359, it has to be checked for undefined angles
+     * after the addition of two angles
+     */
+    private fun calculateAngle(angle: Float): Float {
+        if(angle >= 360) {
+            return (angle - 360)
+        } else if (angle < 0) {
+            return (angle + 360)
+        } else {
+            return angle
         }
     }
 }
--- app/src/main/java/dominikjuergens/example/directiondetectionexample/MainActivity.kt
+++ app/src/main/java/dominikjuergens/example/directiondetectionexample/MainActivity.kt
@@ -2,10 +2,18 @@ package dominikjuergens.example.directiondetectionexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dominikjuergens.example.directiondetectionlibrary.Somda
 
-class MainActivity : AppCompatActivity() {
+abstract class MainActivity : AppCompatActivity(), Somda.SomdaListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val s = Somda(this)
+        s.start(this)
+    }
+
+    override fun onSomdaChanged(degree: Float) {
+        TODO("Not yet implemented")
     }
 }
